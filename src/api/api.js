8051b7f9e1828d8646f8138589c5d739f8aa2698
Carlos Klein <c.edudepaula@yahoo.com.br>
2020-11-23 17:49:27 -0300

    alterações heroku

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://caderninhodigital-api.herokuapp.com/',
+  baseURL: 'https://caderninhodigital-api.herokuapp.com',
   // baseURL: 'http://localhost:8080',
 });
 
